9d52bffdc8f28d7bef6d4c0a803648c31356d99f
Douglas1546 <douglasveras1206@gmail.com>
2026-02-16 10:57:23 -0300

    Fix: Atualizado a conexão de banco de dados padrão para 'censo' e comentado a conexão MySQL não utilizada.

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'censo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,16 +52,16 @@ return [
 
     'connections' => [
 
-        'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST'),
-            'port'      => env('DB_PORT'),
-            'database'  => env('DB_SEMCHAMADA_DATABASE'),
-            'username'  => env('DB_USERNAME'),
-            'password'  => env('DB_PASSWORD'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-        ],
+        // 'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST'),
+        //     'port'      => env('DB_PORT'),
+        //     'database'  => env('DB_SEMCHAMADA_DATABASE'),
+        //     'username'  => env('DB_USERNAME'),
+        //     'password'  => env('DB_PASSWORD'),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        // ],
 
         'compartilhados' => [
             'driver'    => 'mysql',

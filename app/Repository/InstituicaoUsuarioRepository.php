e08e1dc9f103d1b57a9be79807d2a5a3d42393e2
Douglas1546 <douglasveras1206@gmail.com>
2026-02-16 10:06:32 -0300

    Fix: Ajusta flag email_blacklist aplicando MAX() para compatibilidade com GROUP BY

--- app/Repository/InstituicaoUsuarioRepository.php
+++ app/Repository/InstituicaoUsuarioRepository.php
@@ -14,7 +14,7 @@ class InstituicaoUsuarioRepository
                     " . config('database.compartilhados_schema')  .  ".id_usuarios.usua_nome,
                     " . config('database.compartilhados_schema')  .  ".id_usuarios.usua_email,
                     GROUP_CONCAT(" . config('database.censo_schema')  .  ".instituicao_perfil.perf_descricao SEPARATOR ', ') AS usuario_perfil,
-                    IF(" . config('database.email_schema')  .  ".em_black_list.black_list_id IS NULL, 0, 1) AS email_blacklist
+                    IF(MAX(" . config('database.email_schema')  .  ".em_black_list.black_list_id) IS NULL, 0, 1) AS email_blacklist
                 FROM " . config('database.censo_schema')  .  ".instituicao_usuarios
                 INNER JOIN " . config('database.compartilhados_schema')  .  ".id_usuarios ON " . config('database.censo_schema')  .  ".instituicao_usuarios.usua_id = " . config('database.compartilhados_schema')  .  ".id_usuarios.usua_id
                 INNER JOIN " . config('database.censo_schema')  .  ".usuario_perfil ON " . config('database.censo_schema')  .  ".instituicao_usuarios.inst_usua_id = " . config('database.censo_schema')  .  ".usuario_perfil.inst_usua_id
@@ -31,6 +31,6 @@ class InstituicaoUsuarioRepository
                 ORDER BY " . config('database.compartilhados_schema')  .  ".id_usuarios.usua_nome
                 $offset
                 ";
-                return \DB::select($sql, ['inst_codigo' => $inst_codigo]);
+        return \DB::select($sql, ['inst_codigo' => $inst_codigo]);
     }
 }
